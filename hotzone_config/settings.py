f5751588108c94ed7a0f55b2337878f10d120c56
williamchankwokhei <williamchankwokhei@gmail.com>
2020-10-30 12:35:35 +0800

    Bug fix

--- hotzone_config/settings.py
+++ hotzone_config/settings.py
@@ -80,9 +80,7 @@ WSGI_APPLICATION = 'hotzone_config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default': env.dj_db_url('DATABASE_URL')
-    }
+    'default': env.dj_db_url('DATABASE_URL')
 }
 
 
